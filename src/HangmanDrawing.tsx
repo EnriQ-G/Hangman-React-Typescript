e00b254e95ffdb02a42fb320c70b2d0a9729aa7b
Enrique Graniel <legraniel.91@gmail.com>
2024-02-12 19:00:32 -0600

    creación del componente y funcionalidad para aparecer las letras que han sido previamente seleccionadas

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -21,6 +21,7 @@ const HangmanDrawing = () => {
     <div style={{
       width: "10px",
       height: "100px",
+      background: "black",
       position: "absolute",
       top: "120px",
       right: 0,
@@ -32,6 +33,7 @@ const HangmanDrawing = () => {
     <div style={{
       width: "100px",
       height: "10px",
+      background: "black",
       position: "absolute",
       top: "150px",
       right: "-100px",
@@ -45,12 +47,43 @@ const HangmanDrawing = () => {
     <div style={{
       width: "100px",
       height: "10px",
+      background: "black",
       position: "absolute",
       top: "150px",
       right: "10px",
-      rotate: "-30deg",
+      rotate: "30deg",
       transformOrigin: "right bottom",
     }}
+    />
+  )
+
+  const RIGHT_LEG = (
+    <div
+      style={{
+        width: "100px",
+        height: "10px",
+        background: "black",
+        position: "absolute",
+        top: "210px",
+        right: "-90px",
+        rotate: "60deg",
+        transformOrigin: "left bottom",
+      }}
+    />
+  )
+  
+  const LEFT_LEG = (
+    <div
+      style={{
+        width: "100px",
+        height: "10px",
+        background: "black",
+        position: "absolute",
+        top: "210px",
+        right: 0,
+        rotate: "-60deg",
+        transformOrigin: "right bottom",
+      }}
     />
   )
 
@@ -61,12 +94,13 @@ const HangmanDrawing = () => {
       {BODY}
       {RIGHT_ARM}
       {LEFT_ARM}
+      {RIGHT_LEG}
+      {LEFT_LEG}
       <div 
         style={{
           height: "50px", 
           width:"10px", 
           background:"black", 
-          marginLeft:"120px",
           position: "absolute",
           top: 0,
           right: 0,
@@ -78,7 +112,20 @@ const HangmanDrawing = () => {
           background:"black", 
           marginLeft:"120px",
     }}/>
-    <div style={{height:"10px", width:"250px", background:"black"}}/>
+      <div 
+        style={{
+          height: "400px", 
+          width:"10px", 
+          background:"black", 
+          marginLeft:"120px",
+    }}/>
+    <div 
+    style={{
+      height:"10px", 
+      width:"250px", 
+      background:"black"
+      }}
+    />
     </div>
   )
 }
--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
 
 const HangmanWord = () => {
+
+  const word = "test"
+  const guessedLetters = ["t"]
+
   return (
-    <div>HangmanWord</div>
+    <div
+    style={{display: "flex", gap: ".25em", fontSize: "6rem", fontWeight: "bold", textTransform: "uppercase", fontFamily: "moonspace"}}>
+      
+      {word.split("").map((letter, index) =>(
+        <span style={{borderBottom: ".1em solid black"}}>
+          <span
+            style={{
+              visibility: guessedLetters.includes(letter) 
+              ? "visible"
+              : "hidden",
+            }}>
+            {letter}
+          </span>
+          
+        </span>
+        
+      ))}
+    </div>
   )
 }
 
